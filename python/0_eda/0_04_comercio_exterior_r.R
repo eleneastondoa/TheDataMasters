014af7cb11b13e59618021f26fe4a201eab43652
Unai Torrecilla <unai.torrecilla@alumni.mondragon.edu>
2021-04-09 10:13:09 +0200

    Cambios menores

--- python/0_eda/0_04_comercio_exterior_r.R
+++ python/0_eda/0_04_comercio_exterior_r.R
@@ -1,8 +1,11 @@
 #Carga de datos
 getwd()
-df_comercioex <- read.csv('../../datos/datos_originales/Dataset4.- Comercio Exterior de España.txt', 
-                          sep = '|')
+dir_in <- 'datos/datos_originales'
+file_in <- 'Dataset4.- Comercio Exterior de EspaÃ±a.txt'
+df_comercioex <- read.csv(file.path(dir_in, file_in), sep = '|')
+names(df_comercioex)[1] <- 'PERIOD'
 names(df_comercioex)
+
 df_comercioex_spread <- df_comercioex %>% 
   spread('INDICATORS', 'Value')
-write.csv(df_comercioex_spread, '../../datos/datos_desarrollo/comercio_exterior_spread.csv')
+write.csv(df_comercioex_spread, 'datos/datos_desarrollo/comercio_exterior_spread.csv')
